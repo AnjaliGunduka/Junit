c995f2a6347d87177776436cf7e483fb092fe9ad
Anjali Gunduka <anjalig@nbos.vinisoft.com>
2021-09-03 16:48:04 +0530

    addednumbers

--- src/main/java/com/LoSet/LogSetting/App.java
+++ src/main/java/com/LoSet/LogSetting/App.java
@@ -8,8 +8,8 @@ import org.apache.log4j.Logger;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
+
 	final static Logger logger = Logger.getLogger(App.class);
 
 	public static void main(String[] args) {
@@ -36,4 +36,5 @@ public class App
 	public static void sendNotification() {
 		logger.info("Notification Sent");
 	}
+
 }
